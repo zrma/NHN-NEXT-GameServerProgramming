f1a7e67d8e8bcf8ac58765a6f96352ea6b4578e5
#1 DC Shin <dcflight@naver.com>
2014-08-16 23:54:15 +0900

    테스트 하다가 바꿔서 올렸음
    이게 최�

--- HW_05_MT/EduServer_IOCP/PlayerManager.cpp
+++ HW_05_MT/EduServer_IOCP/PlayerManager.cpp
@@ -31,7 +31,7 @@ int PlayerManager::GetCurrentPlayers(PlayerList& outList)
 	//TODO: mLock을 read모드로 접근해서 outList에 현재 플레이어들의 정보를 담고 total에는 현재 플레이어의 총 수를 반환.
 
 	int total = 0;
-	FastSpinlockGuard readMode( mLock );
+	FastSpinlockGuard readMode( mLock, false );
 	
 	
 	for ( auto iter = mPlayerMap.begin(); iter != mPlayerMap.end(); ++iter )
