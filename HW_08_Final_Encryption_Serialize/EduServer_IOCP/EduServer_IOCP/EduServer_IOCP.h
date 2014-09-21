bcd32d23c38a13187154ea219459fe3ea6991df2
#1 DC Shin <dcflight@naver.com>
2014-09-21 16:19:55 +0900

    warning 때려잡기

--- HW_08_Final_Encryption_Serialize/EduServer_IOCP/EduServer_IOCP/EduServer_IOCP.h
+++ HW_08_Final_Encryption_Serialize/EduServer_IOCP/EduServer_IOCP/EduServer_IOCP.h
@@ -2,11 +2,11 @@
 
 //todo: 아래 설정은 여러분들의 상황에 맞게 고치기
 
-#define LISTEN_PORT		9000
+#define LISTEN_PORT		9001
 #define MAX_CONNECTION	10000
 
-#define CONNECT_SERVER_ADDR	"127.0.0.1"
-#define CONNECT_SERVER_PORT 9001
+//#define CONNECT_SERVER_ADDR	"127.0.0.1"
+//#define CONNECT_SERVER_PORT 9001
 
 #define GQCS_TIMEOUT	10 //INFINITE
 
