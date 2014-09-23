6618ecc99ae13d57b42fcdb82e6b36ca320d9e77
#1 DC Shin <dcflight@naver.com>
2014-09-23 23:50:44 +0900

    암호화를 하다가 내가 난독화 되는 상태

---
+++ HW_08_Final_Encryption_Serialize/EduServer_IOCP/EduServer_IOCP/KeyChanger.h
@@ -0,0 +1,36 @@
+#pragma once
+
+#include <tchar.h>
+#include <windows.h>
+#include <wincrypt.h>
+#pragma comment(lib, "crypt32.lib")
+
+struct KeyPrivateSets
+{
+	HCRYPTPROV hProvParty = NULL;
+	HCRYPTKEY hPrivateKey = NULL;
+	HCRYPTKEY hSessionKey = NULL;
+};
+struct KeySendingSets
+{
+	DWORD dwDataLen = -1;
+	PBYTE pbKeyBlob = NULL;
+};
+
+class KeyChanger
+{
+public:
+	KeyChanger();
+	~KeyChanger();
+
+	bool GenerateKey( KeyPrivateSets* keyPrivateSets, KeySendingSets* keySendingSets );
+	bool GetSessionKey( KeyPrivateSets* myPrivateSets, KeySendingSets* othersSendingSets );
+
+	bool EncryptData( HCRYPTKEY sessionKey, BYTE* originalData, int originalSize, BYTE* encryptedData );
+	bool DecryptData( HCRYPTKEY sessionKey, BYTE* encryptedData, int originalSize );
+
+	DATA_BLOB P;
+	DATA_BLOB G;
+
+};
+
